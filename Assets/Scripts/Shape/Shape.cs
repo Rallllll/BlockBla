c1f83220d836d028063c80dcf4da6eb5fe697261
Hoang Trung Nguyen <hoangtrungnguyen149@gmail.com>
2026-06-27 22:38:05 +0700

    tao khay dung shape

--- Assets/Scripts/Shape/Shape.cs
+++ Assets/Scripts/Shape/Shape.cs
@@ -6,14 +6,14 @@ public class Shape : MonoBehaviour
 {
     public GameObject squareShapeImage;
 
+    [HideInInspector]
     public ShapeData CurrentShapeData;
 
     private List<GameObject> _currentShape = new List<GameObject>();
 
-
     void Start()
     {
-        RequestNewShape(CurrentShapeData);
+        
     }
 
     public void RequestNewShape(ShapeData shapeData)
