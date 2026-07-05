dc7157045ea1659be8b155f1bbad0fd003102671
Hoang Trung Nguyen <hoangtrungnguyen149@gmail.com>
2026-07-05 08:48:54 +0700

    ui

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,7 +1,8 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
