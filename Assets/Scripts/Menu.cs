2c2d62ca2f78bbb11c71cc7c5f730732c85bf9ac
Hoang Trung Nguyen <hoangtrungnguyen149@gmail.com>
2026-07-07 21:14:54 +0700

    thêm chức năng exit game

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -22,4 +22,23 @@ public class Menu : MonoBehaviour
         }
         SceneManager.LoadScene(name);
     }
+    // ========================================================
+    // HÀM GẮN VÀO NÚT EXIT (THOÁT GAME)
+    // ========================================================
+    public void ExitGame()
+    {
+        // 1. Phát tiếng bấm nút cho chuyên nghiệp
+        if (SoundEffect.Instance != null) SoundEffect.Instance.PlayClick();
+
+        Debug.Log("Đang thoát game...");
+
+        // 2. Lệnh thoát game thông minh 2 trong 1:
+#if UNITY_EDITOR
+        // Nếu đang ngồi test trong phần mềm Unity -> Tắt nút Play
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // Nếu đã build ra game thật (Android/PC/iOS) -> Tắt hẳn ứng dụng!
+            Application.Quit();
+#endif
+    }
 }
