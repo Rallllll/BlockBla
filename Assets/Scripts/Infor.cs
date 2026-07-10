042cc51d5bcdd53bae8a58e34a47b93d89ab9e01
Hoang Trung Nguyen <hoangtrungnguyen149@gmail.com>
2026-07-10 21:18:29 +0700

    update thêm nút hướng dẫn ở main menu

---
+++ Assets/Scripts/Infor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Infor : MonoBehaviour
+{
+    [Header("--- CÀI ĐẶT UI ---")]
+    public GameObject inFor; // Kéo cái bảng Pop-up bồ đã làm sẵn vào đây
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        // 1. Vừa vào game là tự động ẩn cái Pop-up đi
+        if (inFor != null)
+        {
+            inFor.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick_InforButton()
+    {
+        Debug.Log("ĐÃ BẤM NÚT MỞ! Đang kiểm tra biến inFor..."); // Thêm dòng này
+
+        if (inFor != null)
+        {
+            inFor.SetActive(true);
+            Debug.Log("Mở Pop-up thành công!"); // Thêm dòng này
+        }
+    }
+
+
+    public void OnClick_CancelExit()
+    {
+        // Bấm No hoặc X thì chỉ việc tắt cái bảng Pop-up đi để chơi tiếp
+        if (inFor != null)
+        {
+            inFor.SetActive(false);
+        }
+    }
+}
